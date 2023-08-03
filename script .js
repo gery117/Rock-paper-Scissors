073068246ef3a2f8d4b77b1310e1352538534223
Geraldo Kranenburg <Geraldo.kranenburg@outlook.com>
2023-08-04 02:13:09 +0800

    add function that captures user input for RPS game

--- script .js
+++ script .js
@@ -1,10 +1,42 @@
-function getComputerChoice (){
-    // var choice = Math.floor(Math.random()*3);
-    // console.log(choice);
 
-    var options = ["rock", "paper", "scissors"];
-    var number = Math.floor(Math.random()*options.length)
-    var choice = options[number]
-    console.log(choice);
+function playRound(playerSelection,computerSelection){
+
+    function getComputerChoice (){
+        // var choice = Math.floor(Math.random()*3);
+        // console.log(choice);
+
+        // var options = ["rock", "paper", "scissors"];
+        // var number = Math.floor(Math.random()*options.length)
+        // var choice = options[number]
+        // console.log(choice);
+
+        var options = ["rock", "paper", "scissors"];
+        var choice = Math.floor(Math.random()*options.length);
+
+        // console.log (choice)
+
+        if (choice <= 0){
+            choice = options[0];
+        } 
+        else if (choice <= 1) {
+            choice = options[1];
+        } 
+        else {
+            choice = options[2];
+        }
+        // console.log(choice);
+        return choice
+
+    }
+    // getComputerChoice() 
+
+    function getPlayerChoice(){
+        var userChoice = prompt("please enter a choice",).toLowerCase();
+        // console.log(userChoice);
+        return userChoice
+    }
+    
+    getPlayerChoice()
+
 }
-getComputerChoice() 
+
